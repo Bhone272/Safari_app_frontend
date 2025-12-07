b9ea3c174812021e4d60e4d3f919421c22379332
Bhone272 <bhonemyintmaung272@gmail.com>
2025-12-08 01:34:34 +0630

    Use HashRouter for Vercel deployment

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -7,8 +7,8 @@ import "/src/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
